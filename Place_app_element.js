4ef7939879dfbed2a28c8fc609ddb6dd069bd206
Khanh (Spite) <118985514+ks1610@users.noreply.github.com>
2024-06-30 19:05:04 +0700

    Update Place_app_element.js

--- Place_app_element.js
+++ Place_app_element.js
@@ -1,8 +1,10 @@
 const interval = setInterval(() => {
+  //find the class of the app element
   const append_test = document.querySelector('.app_element')
   console.log(append_test)
   if (append_test) {
     clearInterval(interval)
+    //add block element to PF and add id name
     document.querySelector('.block_id').append(append_test);
   }
 }, 10)
