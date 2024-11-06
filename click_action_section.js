ec3b09191dffdd69f4fbc2eee2c1487a9118c359
Khanh Trinh (Spite) <118985514+ks1610@users.noreply.github.com>
2024-11-06 10:57:29 +0700

    Create click_action_section.js

---
+++ click_action_section.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+const clickySection = document.querySelector("#clicky-section");
+if (clickySection) {
+clickySection.addEventListener("click", function () {
+window.location.href = "https://onlyvintage.de/apps/returns?";
+});
+}
+});
+
