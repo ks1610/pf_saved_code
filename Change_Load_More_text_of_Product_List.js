eeecf2cd4924a8dd2a6267340a3aa762e9fa4c74
Khanh Trinh <118985514+ks1610@users.noreply.github.com>
2025-08-20 21:36:41 +0700

    Create Change_Load_More_text_of_Product_List.js

---
+++ Change_Load_More_text_of_Product_List.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loadMoreLink = document.querySelector(".pf-cmIldm");
+
+  if (loadMoreLink) {
+    loadMoreLink.textContent = "Carica altro";
+  }
+});
